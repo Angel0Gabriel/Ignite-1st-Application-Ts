d193d77918418322df076fffc636dd9bed391300
Ângelo Gabriel <angelobielmoreira@hotmail.com>
2023-01-15 23:46:02 -0300

    converting 1st application from js to ts

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,20 @@
+import styles from './Avatar.module.css';
+
+interface AvatarProps {
+    hasBorder?: boolean;
+    src: string;
+    alt?: string
+}
+
+function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+    return (
+        <img
+            className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+            src={src}
+            alt={alt}
+        />
+
+    );
+}
+
+export default Avatar
