942cd772242e5ee22a7d128da83108b79f9ba258
Angelo Gabriel <angelobielmoreira@hotmail.com>
2023-01-16 13:03:56 -0300

    interface extension

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,16 @@
+import { ImgHTMLAttributes } from 'react';
+
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
     hasBorder?: boolean;
-    src: string;
-    alt?: string
 }
 
-function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+function Avatar({ hasBorder = true, ...props }: AvatarProps) {
     return (
         <img
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-            src={src}
-            alt={alt}
+            {...props}
         />
 
     );
